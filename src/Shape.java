7297c279669ac1a0c7d97500c3bba55950fefce0
cpcsyang <frank.yang@cedarpark.org>
2023-10-26 07:29:08 -0700

    abstract Shape class - unfinished
    trying to rewrite Shape class as abstract

--- src/Shape.java
+++ src/Shape.java
@@ -1,29 +1,23 @@
 import java.awt.*;
 
-public class Shape {
-    private int x = 0, y = 0;
+abstract class Shape {
+    private int x, y;
     private Color color = Color.black;
-    public int getX() {
-        return x;
-    }
-    public int getY() {
-        return y;
-    }
+    public int getX() { return x; }
+    public int getY() { return y; }
     public void setPosition(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public void setColor(Color color) {
-        this.color = color;
-    }
+    public void setColor(Color color) { this.color = color; }
 
-    public Color getColor() {
-        return color;
-    }
+    public Color getColor() { return color; }
 
     public void move(int dx, int dy) {
         x += dx;
         y += dy;
     }
+
+    abstract void add(Graphics g, Shape s);
 }
